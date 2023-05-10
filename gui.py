b312166f67cbaed240df37604f03ecebdbb17e76
nicogno <cogno.nicolo@gmail.com>
2023-05-10 14:43:28 +0200

    Added function for time conversion to seconds

--- gui.py
+++ gui.py
@@ -6,6 +6,23 @@ sg.theme('DarkAmber')   # Add a touch of color
 pace_text = 'Pace (mm:ss)'
 time_text = 'Time (hh:mm:ss)'
 
+def time_to_seconds(time_ : str):
+    time_ = time_.replace(" ", "")  # Trim spaces
+    time_components_ = time_.split(":") # Split
+    components_left_ = len(time_components_)
+    if (components_left_>3 or (components_left_==1 and time_components_[0]=='')):
+        return 0
+    else:
+        time_in_seconds_ = 0
+        power_counter_ = 0
+        while components_left_>0:
+            time_in_seconds_+=int(time_components_[components_left_-1])*pow(60, power_counter_)
+            components_left_-=1
+            power_counter_+=1
+    return time_in_seconds_
+    
+
+
 tab1 = [  [sg.Text('Workout'), sg.VerticalSeparator(k='-VER1-'), sg.Text('Name'), sg.InputText(), sg.VerticalSeparator(k='-VER2-'), sg.Combo(values=('Pace (Min/km) - Distance (km)', 'Pace (Min/mi) - Distance (mi)'), default_value='Pace (Min/km) - Distance (km)', readonly=False, k='-COMBO-'), ],
             [sg.HorizontalSeparator(k='-HOR1-')],
             [sg.Text('Warm up')],
@@ -48,6 +65,7 @@ while True:
             window[key]('')
     if event == 'Compute':
         print('You entered ', values[0])
-        layout[14][1].update('Hello')
+        tab1[14][1].update('Hello')
+        print('Converted time ', time_to_seconds(tab1[3][5].get()))
 
 window.close()
